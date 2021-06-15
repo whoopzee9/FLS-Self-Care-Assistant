6254fb003d1ce78c6e51a02c7ba971d1580cdd61
Fedor Pashin <fedorpashin@icloud.com>
2021-06-15 17:04:34 +0300

    Fix indentation

--- self-care-assistant-ios/Self-Care Assistant/Self_Care_AssistantApp.swift
+++ self-care-assistant-ios/Self-Care Assistant/Self_Care_AssistantApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct Self_Care_AssistantApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
