ae4ce3f5ac431509efe993e4fe68422632e9770d
Denis Korolev <denikorol@mail.ru>
2021-06-23 14:56:30 +0300

    update build.gradles

--- self-care-assistant-backend/build.gradle.kts
+++ self-care-assistant-backend/build.gradle.kts
@@ -57,3 +57,13 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+	jvmTarget = "1.8"
+}
+
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+	jvmTarget = "1.8"
+}
