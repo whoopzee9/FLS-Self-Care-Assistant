3eef6c1b9a11f93c9ba5d020393e85f9cbb59247
Denis Korolev <denikorol@mail.ru>
2021-06-24 11:48:24 +0300

    fix jwt

--- self-care-assistant-backend/src/main/kotlin/selfcareassistant/entity/Constants.kt
+++ self-care-assistant-backend/src/main/kotlin/selfcareassistant/entity/Constants.kt
@@ -1,9 +1,9 @@
 package selfcareassistant.entity
 
 object Constants {
-    const val JWT_SECRET = "eyJhbGciOiJIUzUxMiJ9.ew0KICAic3ViIjogIjEyMzQ1Njc4OTAiLA0KICAibmFtZSI6ICJBbmlzaCBOYXRoI" +
-            "iwNCiAgImlhdCI6IDE1MTYyMzkwMjINCn0.BrDQYqyG8W9Cvi-_zQNeJN2aLUea7YVNvbHu5NPeWZE-SzkkgGTPxukUGHexHE6-FT2Wk9" +
-            "S_fP0Hgh8Jy7xBYQ"
+    const val JWT_SECRET = "eyJhbGciOiJIUzUxMiJ9ew0KICAic3ViIjogIjEyMzQ1Njc4OTAiLA0KICAibmFtZSI6ICJBbmlzaCBOYXRoI" +
+            "iwNCiAgImlhdCI6IDE1MTYyMzkwMjINCn0BrDQYqyG8W9CvizQNeJN2aLUea7YVNvbHu5NPeWZESzkkgGTPxukUGHexHE6FT2Wk9" +
+            "SfP0Hgh8Jy7xBYQ"
     const val JWT_EXPIRATION = 24 * 60 * 1000
 
     const val TOKEN_HEADER = "Authorization"
