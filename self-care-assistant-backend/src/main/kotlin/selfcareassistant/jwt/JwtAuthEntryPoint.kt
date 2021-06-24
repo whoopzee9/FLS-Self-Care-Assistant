c57cc3ca22e9d50aaba6b19a856f362b5d23b9a1
Denis Korolev <denikorol@mail.ru>
2021-06-24 14:05:50 +0300

    fix jwt

--- self-care-assistant-backend/src/main/kotlin/selfcareassistant/jwt/JwtAuthEntryPoint.kt
+++ self-care-assistant-backend/src/main/kotlin/selfcareassistant/jwt/JwtAuthEntryPoint.kt
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.stereotype.Component
-import java.io.IOException
 import javax.servlet.ServletException
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
